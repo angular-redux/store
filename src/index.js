c3d2674fe2caec0145fdac98e862de71ab9dcb95
William Buchwalter <wbuchwalter@gmail.com>
2015-09-02 23:00:51 -0400

    First draft of new api, not functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export {default as ngRedux} from './connector';
+import Connector from './connector';
+import {bind} from 'angular2/di';
+
+export default function createRedux(store) {
+  const _connector = new Connector(store);
+
+  return bind('ngRedux').toFactory(() => {
+    return {connect: _connector.connect, ...store};
+  });
+}
+
+
