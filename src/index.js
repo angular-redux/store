bec2e6493c1b5ba37dd981b1d40420eb7e298042
wbuchwalter <wbuchwalter@gmail.com>
2015-09-03 18:29:26 -0400

    Draft applyInjectableMiddleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import Connector from './connector';
-import {bind} from 'angular2/di';
+import {bind, Injector} from 'angular2/di';
+let redux = import('redux');
 
-export default function createRedux(store) {
+export function createRedux(store) {
   const _connector = new Connector(store);
 
   return bind('ngRedux').toFactory(() => {
     return {connect: _connector.connect, ...store};
   });
 }
+
+
+/*
+ const createStoreWithMiddleware = applyInjectableMiddleware(thunk, 'promise')(createStore);
+*/
+    
+export function applyInjectableMiddleware(middlewares) {
+    const injector = new Injector();
+    let resolvedMiddlewares = [];
+    _.forEach(middlewares, middleware => { 
+        _.isString(middleware) 
+            ? resolvedMiddlewares.push(Injector.resolve(middleware)) 
+            : resolvedMiddlewares.push(middleware)
+    });
+    
+    return redux.applyMiddleware(...resolvedMiddlewares);
+}
+
 
 
