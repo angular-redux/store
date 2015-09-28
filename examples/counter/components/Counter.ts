3868f5ccf2ef5ca9340e38eb31a9ef7caf4f95d0
William Buchwalter <wbuchwalter@gmail.com>
2015-09-28 19:33:03 -0400

    Fix #8

--- examples/counter/components/Counter.ts
+++ examples/counter/components/Counter.ts
@@ -14,10 +14,10 @@ import {Component, View} from 'angular2/angular2';
   template: `
   <p>
     Clicked: {{ counter }} times
-    <button (^click)="increment()">+</button>
-    <button (^click)="decrement()">-</button>
-    <button (^click)="incrementIfOdd()">Increment if odd</button>
-    <button (^click)="incrementAsync()">Increment async</button>
+    <button (click)="increment()">+</button>
+    <button (click)="decrement()">-</button>
+    <button (click)="incrementIfOdd()">Increment if odd</button>
+    <button (click)="incrementAsync()">Increment async</button>
   </p>
   `
 })
