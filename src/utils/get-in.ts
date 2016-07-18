c19f87d989fee0017eb6f35c3599f079a42b6e30
Chris Bond <cb@clbond.org>
2016-07-18 10:36:53 -0400

    Add seamless support for ImmutableJS structures in select() path (#160)
    * Add seamless support for ImmutableJS structures in select() path
    
    * Do not depend upon immutablejs, only use it if the project uses it
    
    * Add comment to make clear the purpose of the try/catch
    
    * Add unit tests related to optional ImmutableJS support

--- src/utils/get-in.ts
+++ src/utils/get-in.ts
@@ -1,3 +1,11 @@
+// This may look suspicious, but the point is to add ImmutableJS integration
+// only if the project itself already has a dependency on immutable. If not,
+// then this variable is left null and no integration is attempted.
+let immutable;
+try {
+  immutable = require('immutable');
+} catch (e) {}
+
 /*
  * Gets a deeply-nested property value from an object, given a 'path'
  * of property names or array indices.
@@ -5,10 +13,15 @@
 export function getIn(
   v: Object,
   pathElems: (string | number)[]): any {
-    const [ firstElem, ...restElems] = pathElems;
     if (!v) {
       return v;
     }
+
+    if (immutable && immutable.Iterable.isIterable(v)) {
+      return (<{getIn: (path: (string | number)[]) => any}>v).getIn(pathElems);
+    }
+
+    const [ firstElem, ...restElems] = pathElems;
 
     if (undefined === v[firstElem]) {
       return undefined;
@@ -20,3 +33,4 @@ export function getIn(
 
     return getIn(v[firstElem], restElems);
 }
+
