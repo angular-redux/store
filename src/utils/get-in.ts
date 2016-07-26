bd1653de7009c33b81d720f8597032fa045b055a
Chris Bond <cb@clbond.org>
2016-07-26 09:29:35 -0400

    Change immutablejs integration approach (#169)
    * Change immutablejs integration approach
    
    * Return null if getIn returns null

--- src/utils/get-in.ts
+++ src/utils/get-in.ts
@@ -1,24 +1,15 @@
-// This may look suspicious, but the point is to add ImmutableJS integration
-// only if the project itself already has a dependency on immutable. If not,
-// then this variable is left null and no integration is attempted.
-let immutable;
-try {
-  immutable = require('immutable');
-} catch (e) {}
-
 /*
  * Gets a deeply-nested property value from an object, given a 'path'
  * of property names or array indices.
  */
-export function getIn(
-  v: Object,
-  pathElems: (string | number)[]): any {
+export function getIn(v, pathElems: (string | number)[]): any {
     if (!v) {
       return v;
     }
 
-    if (immutable && immutable.Iterable.isIterable(v)) {
-      return (<{getIn: (path: (string | number)[]) => any}>v).getIn(pathElems);
+    // If this is an ImmutableJS structure, use existing getIn function
+    if (typeof v.getIn === 'function') {
+      return v.getIn(pathElems);
     }
 
     const [ firstElem, ...restElems] = pathElems;
