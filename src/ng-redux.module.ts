fd7eea0c2245e602022542bf00d732590140ae7d
Evan Schultz <evan@rangle.io>
2016-12-24 11:15:20 -0500

    Revert "Fixes build with ngc 2.3.x" (#285)

--- src/ng-redux.module.ts
+++ src/ng-redux.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { NgRedux } from './components/ng-redux';
 import { DevToolsExtension } from './components/dev-tools';
 
+@NgModule({})
+export class NgReduxModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: NgReduxModule,
+      providers: provideRedux(),
+    };
+  }
+};
 
 export function _ngReduxFactory() {
   return new NgRedux(null);
 }
 
-@NgModule({
-  providers: [
+export function provideRedux(): any[] {
+  return [
+    { provide: NgRedux, useFactory: _ngReduxFactory },
     DevToolsExtension,
-    { provide: NgRedux, useFactory: _ngReduxFactory }
-  ]
-})
-export class NgReduxModule { };
+  ];
+}
